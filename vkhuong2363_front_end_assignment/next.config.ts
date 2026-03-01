10843fae7a09c6ff047631f853206f612a891f5b
minhquan38 <khuongvuminhquan38@gmail.com>
2026-03-01 18:46:36 -0500

    Trying to deploy the app using this tutorial:https://www.freecodecamp.org/news/how-to-deploy-next-js-app-to-github-pages/

--- vkhuong2363_front_end_assignment/next.config.ts
+++ vkhuong2363_front_end_assignment/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/frontend-assignment-minhquan38/tree/main/vkhuong2363_front_end_assignment",
+  output: "export",  
+  reactStrictMode: true,
 };
 
 export default nextConfig;
